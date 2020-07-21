797a42caa56e1b08300c38f9fcb35c69a055f07e
maromaroXD <amine-fb-fb@hotmail.fr>
2020-07-21 13:18:23 +0100

    show avatar image

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+async function showAvatar() {
 
 
-async function test(){
-  return 1;
+  let response = await fetch('user.json');
+  let user = await response.json();
+
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
+
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
 }
 
-test()
-  .then(console.log)
+module.exports = {
+  showAvatar    : showAvatar
+};
